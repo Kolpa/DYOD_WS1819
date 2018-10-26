1f6ad72ca2c1e626d773d4fd29e9d2a600034b62
Marcel <marcel.weisgut@student.hpi.de>
2018-10-26 16:39:10 +0200

    code style improvements

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -19,14 +19,16 @@ void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) { _columns.push_ba
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
   DebugAssert(values.size() == _columns.size(),
               "Number of passed arguments does not equal the number of stored columns.");
-  for (std::size_t index = 0; index < values.size(); ++index) {
-    _columns.at(index).get()->append(values.at(index));
+  auto value_iter = values.cbegin();
+  for (auto const& column : _columns) {
+    column->append(*value_iter);
+    ++value_iter;
   }
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
   if (_columns.size() > column_id.t) {
-    return _columns.at(column_id.t);
+    return _columns[column_id.t];
   }
   return nullptr;
 }
@@ -34,10 +36,10 @@ std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
 uint16_t Chunk::column_count() const { return _columns.size(); }
 
 uint32_t Chunk::size() const {
-  if (_columns.size() == 0) {
+  if (_columns.empty()) {
     return 0;
   } else {
-    return _columns.at(0).get()->size();
+    return _columns[0]->size();
   }
 }
 
