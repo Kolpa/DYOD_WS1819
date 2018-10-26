8b58f7a3ed160aa51438f3b2b26f0b47c97bfdba
Marcel <marcel.weisgut@student.hpi.de>
2018-10-26 11:49:05 +0200

    added chunk implementation, tests passed

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -14,27 +14,31 @@
 
 namespace opossum {
 
-void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) {
-  // Implementation goes here
-}
+void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) { _columns.push_back(segment); }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  // Implementation goes here
+  DebugAssert(values.size() == _columns.size(),
+              "Number of passed arguments does not equal the number of stored columns.");
+  for (std::size_t index = 0; index < values.size(); ++index) {
+    _columns.at(index).get()->append(values.at(index));
+  }
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
-  // Implementation goes here
+  if (_columns.size() > column_id.t) {
+    return _columns.at(column_id.t);
+  }
   return nullptr;
 }
 
-uint16_t Chunk::column_count() const {
-  // Implementation goes here
-  return 0;
-}
+uint16_t Chunk::column_count() const { return _columns.size(); }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+  if (_columns.size() == 0) {
+    return 0;
+  } else {
+    return _columns.at(0).get()->size();
+  }
 }
 
 }  // namespace opossum
