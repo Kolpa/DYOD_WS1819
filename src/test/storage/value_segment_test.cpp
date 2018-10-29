bc675f64cae3c00dd8915ab9853b0998e0bfa478
mapp <mflueggen@student.hpi.de>
2018-10-29 11:20:41 -0400

    Adds additional tests for ValueSegment

--- src/test/storage/value_segment_test.cpp
+++ src/test/storage/value_segment_test.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <limits>
 #include <string>
 #include <vector>
@@ -45,6 +46,33 @@ TEST_F(StorageValueSegmentTest, AddValueOfDifferentType) {
   double_value_segment.append(4);
   EXPECT_EQ(double_value_segment.size(), 1u);
   EXPECT_THROW(double_value_segment.append("Hi"), std::exception);
+}
+
+TEST_F(StorageValueSegmentTest, DirectValueIndexing) {
+  int_value_segment.append(3);
+  EXPECT_EQ(type_cast<int>(int_value_segment[0]), 3);
+  int_value_segment.append(42);
+  EXPECT_EQ(type_cast<int>(int_value_segment[1]), 42);
+
+  string_value_segment.append("Develop");
+  EXPECT_EQ(type_cast<std::string>(string_value_segment[0]), "Develop");
+  string_value_segment.append("Your");
+  EXPECT_EQ(type_cast<std::string>(string_value_segment[1]), "Your");
+
+  double_value_segment.append(3.14);
+  EXPECT_EQ(type_cast<double>(double_value_segment[0]), 3.14);
+  double_value_segment.append(4.44);
+  EXPECT_EQ(type_cast<double>(double_value_segment[1]), 4.44);
+}
+
+TEST_F(StorageValueSegmentTest, GetValues) {
+  std::vector<int> numbers{1, 4, 4, 8, 0};
+
+  for (const auto n : numbers) {
+    int_value_segment.append(n);
+  }
+
+  EXPECT_TRUE(std::equal(numbers.cbegin(), numbers.cend(), int_value_segment.values().cbegin()));
 }
 
 }  // namespace opossum
