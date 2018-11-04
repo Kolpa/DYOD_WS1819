929c1fcce7e48592d06724c8f10a9381f5f9678a
Marcel <marcel.weisgut@student.hpi.de>
2018-11-04 12:16:00 +0100

    added dict and att vect initialization

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -10,14 +10,31 @@
 
 
 int main() {
-  opossum::Assert(true, "We can use opossum files here :)");
-  opossum::FittedAttributeVector<uint8_t> f1;
-  std::cout << static_cast<uint32_t>(f1.width()) << std::endl;
-  opossum::FittedAttributeVector<uint16_t> f2;
-  std::cout << static_cast<uint32_t>(f2.width()) << std::endl;
-  opossum::FittedAttributeVector<uint32_t> f3;
-  std::cout << static_cast<uint32_t>(f3.width()) << std::endl;
-  opossum::FittedAttributeVector<uint64_t> f4;
-  std::cout << static_cast<uint32_t>(f4.width()) << std::endl;
+//  opossum::Assert(true, "We can use opossum files here :)");
+//  opossum::FittedAttributeVector<uint8_t> f1;
+//  std::cout << static_cast<uint32_t>(f1.width()) << std::endl;
+//  opossum::FittedAttributeVector<uint16_t> f2;
+//  std::cout << static_cast<uint32_t>(f2.width()) << std::endl;
+//  opossum::FittedAttributeVector<uint32_t> f3;
+//  std::cout << static_cast<uint32_t>(f3.width()) << std::endl;
+//  opossum::FittedAttributeVector<uint64_t> f4;
+//  std::cout << static_cast<uint32_t>(f4.width()) << std::endl;
+
+  std::vector<int> vector;
+
+  int32_t x = 5;
+
+  auto iter = vector.cbegin();
+  do{
+    std::cout << "iter: " << (iter.base()) << std::endl;
+    std::cout << "end: " << (vector.cend().base()) << std::endl;
+    if(iter == vector.cend() || x < *iter){
+      iter = vector.insert(iter, x);
+    }
+    ++iter;
+    std::cout << "iter: " << (iter.base()) << std::endl;
+    std::cout << "end: " << (vector.cend().base()) << std::endl;
+  }while(iter != vector.cend());
+
   return 0;
 }
--- src/lib/storage/dictionary_segment.cpp
+++ src/lib/storage/dictionary_segment.cpp
@@ -15,8 +15,8 @@ namespace opossum {
 template <class T>
 DictionarySegment<T>::DictionarySegment(const std::shared_ptr<BaseSegment>& base_segment) {
   const auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(base_segment);
-  //TODO(Marcel) build sorted dictionary
-  //TODO(Marcel) attribute vector
+  _init_dictionary(value_segment);
+  _init_attribute_vector(value_segment);
 }
 
 // SEMINAR INFORMATION: Since most of these methods depend on the template parameter, you will have to implement
@@ -98,12 +98,21 @@ size_t DictionarySegment<T>::size() const {
 
 template <typename T>
 void DictionarySegment<T>::_init_dictionary(const std::shared_ptr<ValueSegment<T>>& value_segment) {
-
+  for(const auto& seg_value: value_segment->values()){
+    auto dict_iter = _dictionary->cbegin();
+    do{
+      // insert the segment value if the end of dictionary is reached or
+      // if the segment value is less than the current iterated dict value
+      if(dict_iter == _dictionary->cend() || seg_value < *dict_iter){
+        dict_iter = _dictionary->insert(dict_iter, seg_value);
+      }
+      ++dict_iter;
+    }while((dict_iter != _dictionary->cend()));
+  }
 }
 
 template <typename T>
 void DictionarySegment<T>::_init_attribute_vector(const std::shared_ptr<ValueSegment<T>>& value_segment) {
-  //TODO(Marcel)
   if(value_segment->size() <= std::numeric_limits<uint8_t>::max()){
     _attribute_vector = std::make_shared<FittedAttributeVector<uint8_t >>();
   }else if(value_segment->size() <= std::numeric_limits<uint16_t>::max()){
@@ -112,6 +121,14 @@ void DictionarySegment<T>::_init_attribute_vector(const std::shared_ptr<ValueSeg
     _attribute_vector = std::make_shared<FittedAttributeVector<uint32_t >>();
   }
 
+  ChunkOffset att_vec_index = 0;
+  for(const auto& value : value_segment->values()){
+    const auto occurence_iter = std::find(_dictionary->cbegin(), _dictionary->cend(), value);
+    DebugAssert(occurence_iter != _dictionary->cend(), "Value is not available in the dictionary.");
+    uint32_t dict_pos = std::distance(_dictionary->cbegin(), occurence_iter);
+    _attribute_vector->set(att_vec_index, ValueID{dict_pos});
+    ++att_vec_index;
+  }
 }
 
    EXPLICITLY_INSTANTIATE_DATA_TYPES(DictionarySegment);
