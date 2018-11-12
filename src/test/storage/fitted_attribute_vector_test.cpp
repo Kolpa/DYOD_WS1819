522e46a724de384d6d6df2654a6843c0191f5986
Kolya Opahle <kolyaopahle@gmail.com>
2018-11-12 14:14:28 +0100

    added fitted attribute vector tests

---
+++ src/test/storage/fitted_attribute_vector_test.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+#include <string>
+
+#include "../base_test.hpp"
+#include "gtest/gtest.h"
+
+#include "../lib/storage/fitted_attribute_vector.hpp"
+
+namespace opossum {
+
+class FittedAttributeVectorTest : public BaseTest {
+ protected:
+  void SetUp() override {
+    std::vector<uint8_t> base_data = {1,2,3,4,5,6,7,8,9,10};
+    attr_vect = std::make_shared<FittedAttributeVector<uint8_t>>(std::move(base_data));
+  }
+  std::shared_ptr<FittedAttributeVector<uint8_t>> attr_vect = nullptr;
+};
+
+TEST_F(FittedAttributeVectorTest, SetAndGetVectorValueID) {
+    EXPECT_EQ(attr_vect->get(0), (opossum::ValueID)1);
+    attr_vect->set(0, (opossum::ValueID)2);
+    EXPECT_EQ(attr_vect->get(0), (opossum::ValueID)2);
+}
+
+TEST_F(FittedAttributeVectorTest, GetVectorWidth) {
+    EXPECT_EQ(attr_vect->width(), sizeof(uint8_t));
+}
+
+}  // namespace opossum
