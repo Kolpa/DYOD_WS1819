32dc0aada6fd4c81a96452a24a850333f096b0ac
Marcel <marcel.weisgut@student.hpi.de>
2018-11-27 00:21:59 +0100

    formatting, added missing libraries

--- src/lib/operators/table_scan_impl.hpp
+++ src/lib/operators/table_scan_impl.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <utility>
+#include <vector>
 
 #include "base_table_scan_impl.hpp"
 #include "storage/reference_segment.hpp"
@@ -50,9 +52,10 @@ class TableScanImpl : public BaseTableScanImpl {
     for (ChunkID chunk_id{0}; chunk_id < _input_table->chunk_count(); ++chunk_id) {
       const auto segment_to_scan = _input_table->get_chunk(chunk_id).get_segment(_column_id);
 
-      const std::shared_ptr<ReferenceSegment> ref_segment = std::dynamic_pointer_cast<ReferenceSegment>(segment_to_scan);
+      const std::shared_ptr<ReferenceSegment> ref_segment =
+          std::dynamic_pointer_cast<ReferenceSegment>(segment_to_scan);
 
-      if(ref_segment == nullptr) {
+      if (ref_segment == nullptr) {
         // non ref segment
         const std::shared_ptr<PosList> pos_list = _get_positions_of_accepted_values(chunk_id, segment_to_scan);
 
@@ -60,7 +63,7 @@ class TableScanImpl : public BaseTableScanImpl {
           auto chunk_to_add = _create_chunk(pos_list);
           output_table->emplace_chunk(std::move(chunk_to_add));
         }
-      }else{
+      } else {
         // ref semgent
         const std::shared_ptr<PosList> pos_list = _get_positions_of_accepted_values(chunk_id, ref_segment);
 
@@ -116,32 +119,32 @@ class TableScanImpl : public BaseTableScanImpl {
 
   // gets the positions of accepted values for a value segment of dict. segment
   std::shared_ptr<PosList> _get_positions_of_accepted_values(const ChunkID& chunk_id,
-                                                             const std::shared_ptr<BaseSegment>& segment) const{
-      std::shared_ptr<PosList> pos_list = std::make_shared<PosList>();
-      pos_list->reserve(segment->size() * POS_LIST_RESERVATION_FACTOR);
+                                                             const std::shared_ptr<BaseSegment>& segment) const {
+    std::shared_ptr<PosList> pos_list = std::make_shared<PosList>();
+    pos_list->reserve(segment->size() * POS_LIST_RESERVATION_FACTOR);
 
-      // Find out out the type of the segment by using dynamic pointer casts.
-      const auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(segment);
-      if (value_segment != nullptr) {
-          return _get_positions_of_accepted_values(chunk_id, value_segment, pos_list);
-      }
+    // Find out out the type of the segment by using dynamic pointer casts.
+    const auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(segment);
+    if (value_segment != nullptr) {
+      return _get_positions_of_accepted_values(chunk_id, value_segment, pos_list);
+    }
 
-      const auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(segment);
-      if (dictionary_segment != nullptr) {
-          return _get_positions_of_accepted_values(chunk_id, dictionary_segment, pos_list);
-       }
+    const auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(segment);
+    if (dictionary_segment != nullptr) {
+      return _get_positions_of_accepted_values(chunk_id, dictionary_segment, pos_list);
+    }
 
-      throw std::runtime_error("Unsupported segment type.");
+    throw std::runtime_error("Unsupported segment type.");
   }
 
   // gets the positions of accepted values for a value segment of dict. segment
   std::shared_ptr<PosList> _get_positions_of_accepted_values(const std::shared_ptr<BaseSegment>& segment,
                                                              std::shared_ptr<PosList>& target_list,
-                                                             const std::shared_ptr<PosList>& positions_to_scan) const{
+                                                             const std::shared_ptr<PosList>& positions_to_scan) const {
     // Find out out the type of the segment by using dynamic pointer casts.
     const auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(segment);
     if (value_segment != nullptr) {
-        return _get_positions_of_accepted_values(value_segment, target_list, positions_to_scan);
+      return _get_positions_of_accepted_values(value_segment, target_list, positions_to_scan);
     }
 
     const auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(segment);
@@ -166,24 +169,23 @@ class TableScanImpl : public BaseTableScanImpl {
     return target_list;
   }
 
-    std::shared_ptr<PosList> _get_positions_of_accepted_values(const std::shared_ptr<ValueSegment<T>>& segment,
-                                                               std::shared_ptr<PosList> target_list,
-                                                               const std::shared_ptr<PosList> positions_to_scan) const {
+  std::shared_ptr<PosList> _get_positions_of_accepted_values(const std::shared_ptr<ValueSegment<T>>& segment,
+                                                             std::shared_ptr<PosList> target_list,
+                                                             const std::shared_ptr<PosList> positions_to_scan) const {
     const auto& values = segment->values();
-    for(const auto& row_id : *positions_to_scan){
+    for (const auto& row_id : *positions_to_scan) {
       const auto& value = values[row_id.chunk_offset];
-        if(_accepted_by_comparison(value)){
-          target_list->emplace_back(row_id);
-        }
+      if (_accepted_by_comparison(value)) {
+        target_list->emplace_back(row_id);
       }
-
-      return target_list;
     }
 
-  std::shared_ptr<PosList> _get_positions_of_accepted_values(const ChunkID& chunkID,
-          const std::shared_ptr<DictionarySegment<T>>& segment,
-          std::shared_ptr<PosList> target_list) const {
+    return target_list;
+  }
 
+  std::shared_ptr<PosList> _get_positions_of_accepted_values(const ChunkID& chunkID,
+                                                             const std::shared_ptr<DictionarySegment<T>>& segment,
+                                                             std::shared_ptr<PosList> target_list) const {
     const auto& attribute_vector = segment->attribute_vector();
 
     for (size_t att_vec_offset = 0; att_vec_offset < attribute_vector->size(); ++att_vec_offset) {
@@ -196,23 +198,23 @@ class TableScanImpl : public BaseTableScanImpl {
     return target_list;
   }
 
-    std::shared_ptr<PosList> _get_positions_of_accepted_values(const std::shared_ptr<DictionarySegment<T>>& segment,
-                                                               std::shared_ptr<PosList> target_list,
-                                                               const std::shared_ptr<PosList> positions_to_scan) const {
-      const auto& attribute_vector = segment->attribute_vector();
+  std::shared_ptr<PosList> _get_positions_of_accepted_values(const std::shared_ptr<DictionarySegment<T>>& segment,
+                                                             std::shared_ptr<PosList> target_list,
+                                                             const std::shared_ptr<PosList> positions_to_scan) const {
+    const auto& attribute_vector = segment->attribute_vector();
 
-      for(const auto& row_id : *positions_to_scan){
-        const auto& value = segment->value_by_value_id(attribute_vector->get(row_id.chunk_offset));
-        if (_accepted_by_comparison(value)) {
-          target_list->emplace_back(row_id);
-        }
+    for (const auto& row_id : *positions_to_scan) {
+      const auto& value = segment->value_by_value_id(attribute_vector->get(row_id.chunk_offset));
+      if (_accepted_by_comparison(value)) {
+        target_list->emplace_back(row_id);
       }
-
-      return target_list;
     }
 
-  std::shared_ptr<PosList> _get_positions_of_accepted_values(const ChunkID& chunkID,
-                                                             const std::shared_ptr<ReferenceSegment>& ref_segment) const {
+    return target_list;
+  }
+
+  std::shared_ptr<PosList> _get_positions_of_accepted_values(
+      const ChunkID& chunkID, const std::shared_ptr<ReferenceSegment>& ref_segment) const {
     std::shared_ptr<PosList> pos_list = std::make_shared<PosList>();
     pos_list->reserve(ref_segment->size() * POS_LIST_RESERVATION_FACTOR);
     // list of PosLists, each pos list refers to only one chunk
@@ -220,36 +222,38 @@ class TableScanImpl : public BaseTableScanImpl {
 
     // iterate through all the single chunk pos lists
     const auto& segment_to_scan = ref_segment->referenced_table()->get_chunk(chunkID).get_segment(_column_id);
-    for(const auto& positions_to_scan : *single_chunk_pos_lists){
+    for (const auto& positions_to_scan : *single_chunk_pos_lists) {
       pos_list = _get_positions_of_accepted_values(segment_to_scan, pos_list, positions_to_scan);
     }
 
     return pos_list;
   }
 
-  // this method takes a PosList and creates a list of PosLists so that every PosList is just referenced to a single chunk.
-  const std::shared_ptr<std::vector<std::shared_ptr<PosList>>> _get_single_chunk_pos_lists(const std::shared_ptr<const PosList>& pos_list) const {
-    std::shared_ptr<std::vector<std::shared_ptr<PosList>>> single_chunk_pos_lists = std::make_shared<std::vector<std::shared_ptr<PosList>>>();
+  // this method takes a PosList and creates a list of PosLists so that every PosList
+  // is just referenced to a single chunk.
+  const std::shared_ptr<std::vector<std::shared_ptr<PosList>>> _get_single_chunk_pos_lists(
+      const std::shared_ptr<const PosList>& pos_list) const {
+    std::shared_ptr<std::vector<std::shared_ptr<PosList>>> single_chunk_pos_lists =
+        std::make_shared<std::vector<std::shared_ptr<PosList>>>();
     std::map<ChunkID, uint32_t> chunk_id_to_list_index;
 
-    for(const auto& row_id : *pos_list){
+    for (const auto& row_id : *pos_list) {
       // Chunk with id of currently iterated row id was previously not processed in this loop.
       const auto pos_lists_index_iter = chunk_id_to_list_index.find(row_id.chunk_id);
 
-      if(pos_lists_index_iter == chunk_id_to_list_index.cend()){
+      if (pos_lists_index_iter == chunk_id_to_list_index.cend()) {
         // a PosList for the ChunkId of currently iterated RowId exits in the list of PosLists
         auto new_pos_list = std::make_shared<PosList>();
         new_pos_list->emplace_back(row_id);
         single_chunk_pos_lists->emplace_back(new_pos_list);
-        chunk_id_to_list_index[row_id.chunk_id] = single_chunk_pos_lists->size()-1;
-      } else{
+        chunk_id_to_list_index[row_id.chunk_id] = single_chunk_pos_lists->size() - 1;
+      } else {
         // a PosList for the ChunkId of currently iterated RowId exits in the list of PosLists
         single_chunk_pos_lists->at(pos_lists_index_iter->second)->emplace_back(row_id);
       }
     }
     return single_chunk_pos_lists;
   }
-
 };
 
 }  // namespace opossum
