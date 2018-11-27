684b7700ebaa58aff34c03c36b65b66e7533d807
mapp <mflueggen@student.hpi.de>
2018-11-27 16:52:13 -0500

    Adds comments.

--- src/lib/operators/segment_scanner.hpp
+++ src/lib/operators/segment_scanner.hpp
@@ -100,15 +100,25 @@ class NotEqualsScanner;
  * @tparam T Type of the values the scanner will operate on,
  */
 template <typename T>
-class AbstractSegmentScanner {
+class AbstractSegmentScanner : private Noncopyable {
  public:
   virtual ~AbstractSegmentScanner() = default;
 
+  /**
+   * Compares value with cmp_value. Let cmp be a binary comperator.
+   * Returns true if "value cmp cmp_value" evaluates to true.
+   */
   virtual bool compare(const T& value, const T& cmp_value) = 0;
 
+  /**
+   * Scans segment and returns a PosList with all RowsIds for which the compare function
+   * yields true.
+   * chunk_id is ignored if segment is a ReferenceSegment
+   */
   PosList scan(const ChunkID chunk_id, const std::shared_ptr<BaseSegment>& segment, const T& cmp_value) {
+    // Determine dynamic type of segment and forward to specialized scan implementation.
     if (const auto& reference_segment = std::dynamic_pointer_cast<ReferenceSegment>(segment)) {
-      return scan(chunk_id, *reference_segment, cmp_value);
+      return scan(*reference_segment, cmp_value);
     } else if (const auto& value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(segment)) {
       return scan(chunk_id, *value_segment, cmp_value);
     } else if (const auto& dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(segment)) {
@@ -118,34 +128,53 @@ class AbstractSegmentScanner {
     throw std::runtime_error("Unsupported segment type.");
   }
 
+  /**
+   * Scans a DictionarySegment and returns a PosList with all RowsIds for which the compare function
+   * yields true.
+   */
   PosList scan(const ChunkID chunk_id, const DictionarySegment<T>& segment, const T& cmp_value) {
     auto index_fetcher = ContinuousIndexFetcher(0, segment.size());
     return this->template scan<ContinuousIndexFetcher>(chunk_id, segment, cmp_value, index_fetcher);
   }
 
+  /**
+   * Scans a ValueSegment and returns a PosList with all RowsIds for which the compare function
+   * yields true.
+   */
   PosList scan(const ChunkID chunk_id, const ValueSegment<T>& segment, const T& cmp_value) {
     auto index_fetcher = ContinuousIndexFetcher(0, segment.size());
     return this->template scan<ContinuousIndexFetcher>(chunk_id, segment, cmp_value, index_fetcher);
   }
 
-  PosList scan(const ChunkID chunk_id, const ReferenceSegment& segment, const T& cmp_value) {
+  /**
+   * Scans a ReferenceSegment and returns a PosList with all RowsIds for which the compare function
+   * yields true.
+   */
+  PosList scan(const ReferenceSegment& segment, const T& cmp_value) {
     const auto& pos_list = *segment.pos_list();
     if (pos_list.empty()) {
       return PosList{};
     }
 
     const auto& table = segment.referenced_table();
+    // PosList to hold the selected RowIDs
     PosList result;
 
     size_t start_index = 0;
     auto last_chunk_id = pos_list[0].chunk_id;
 
+    // We imply that the PosList is sorted by chunk id. If not, a performance penalty could be observed.
+    // Iterate over pos_list() of segment and search for a continuous sequence of RowIds with the same
+    // chunk id. As soon as the chunk ids of two neighboring elements within pos_list() do not match
+    // scan the segment corresponding to the chunk id. This is neccessary to optimize the value
+    // selection within the segment which can be either a Value- or DictionarySegment.
     for (size_t index = 1; index < pos_list.size(); ++index) {
       const auto& row_id = pos_list[index];
       if (row_id.chunk_id != last_chunk_id) {
         const auto& chunk = table->get_chunk(last_chunk_id);
         const auto base_segment = chunk.get_segment(segment.referenced_column_id());
-        const auto tmp_result = scan(chunk_id, base_segment, cmp_value, pos_list, start_index, index);
+        const auto tmp_result = scan(last_chunk_id, base_segment, cmp_value, pos_list, start_index, index);
+        // Move elements from tmp_result to the end of result
         result.insert(result.end(), std::make_move_iterator(tmp_result.begin()),
                       std::make_move_iterator(tmp_result.end()));
         start_index = index;
@@ -153,10 +182,12 @@ class AbstractSegmentScanner {
       }
     }
 
-    {  // Exact duplicate to lines above to add range from [start_index, pos_list.size())
+    // Exact duplicate to lines above include the entries in pos_list() belonging to the last chunk.
+    {
       const auto& chunk = table->get_chunk(last_chunk_id);
       const auto base_segment = chunk.get_segment(segment.referenced_column_id());
-      const auto tmp_result = scan(chunk_id, base_segment, cmp_value, pos_list, start_index, pos_list.size());
+      const auto tmp_result = scan(last_chunk_id, base_segment, cmp_value, pos_list, start_index, pos_list.size());
+      // Move elements from tmp_result to the end of result
       result.insert(result.end(), std::make_move_iterator(tmp_result.begin()),
                     std::make_move_iterator(tmp_result.end()));
     }
@@ -164,6 +195,9 @@ class AbstractSegmentScanner {
     return result;
   }
 
+  /**
+   * Helper function to create the correct scanner given a scan_type.
+   */
   static std::unique_ptr<AbstractSegmentScanner> from_scan_type(ScanType scan_type) {
     switch (scan_type) {
       case ScanType::OpEquals:
@@ -186,11 +220,25 @@ class AbstractSegmentScanner {
  protected:
   virtual bool compare_by_value_id(const ValueID& value_id, const ValueID& cmp_value_value_id) = 0;
 
+  /**
+   * Gets a value id to a value within a DictionarySegment, depending on the concrete implementation
+   */
   virtual ValueID get_value_id(const DictionarySegment<T>& segment, const T& value) = 0;
 
+  /**
+   * Scans a BaseSegment
+   * @param chunk_id ChunkId to segment which is scanned.
+   * @param segment Segment to be scanned
+   * @param cmp_value Value to comparte values to
+   * @param pos_list List of positions to evaluate
+   * @param start_index start index within pos_list
+   * @param end_index end_index (excluding) within pos_list
+   * @return A PosList with the selected Rows
+   */
   PosList scan(const ChunkID chunk_id, const std::shared_ptr<BaseSegment>& segment, const T& cmp_value,
                const PosList& pos_list, size_t start_index, size_t end_index) {
     auto index_fetcher = PosListIndexFetcher(start_index, end_index, pos_list);
+    // Determine dynamic type of segment and forward to concrete implementation.
     if (const auto& value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(segment)) {
       return this->template scan<PosListIndexFetcher>(chunk_id, *value_segment, cmp_value, index_fetcher);
     } else if (const auto& dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(segment)) {
@@ -199,13 +247,18 @@ class AbstractSegmentScanner {
     throw std::runtime_error("Unsupported segment type.");
   }
 
+  /**
+   * Concrete implementation for scanning a DictionarySegment.
+   * @tparam IndexFetcher Type of index fetcher to use. This way we don't have to duplicate our code
+   * to iterate over a ReferenceSegment or DictionarySegment.
+   */
   template <typename IndexFetcher>
   PosList scan(const ChunkID chunk_id, const DictionarySegment<T>& segment, const T& cmp_value,
                IndexFetcher& index_fetcher) {
     const auto value_id_to_compare_to = get_value_id(segment, cmp_value);
 
-    // Todo: there are cases where we could have a select all or select none case here.
-    // say a avlue is not in the dictionary.
+    // Todo: There are cases where we could have a select all or select none case here,
+    // for example if a value cannot be found within the dictionary.
     PosList pos_list;
 
     const auto& attribute_vector = segment.attribute_vector();
@@ -221,6 +274,11 @@ class AbstractSegmentScanner {
     return pos_list;
   }
 
+  /**
+   * Concrete implementation for scanning a ValueSegment.
+   * @tparam IndexFetcher Type of index fetcher to use. This way we don't have to duplicate our code
+   * to iterate over a ReferenceSegment or ValueSegment.
+   */
   template <typename IndexFetcher>
   PosList scan(const ChunkID chunk_id, const ValueSegment<T>& segment, const T& cmp_value,
                IndexFetcher& index_fetcher) {
@@ -245,6 +303,9 @@ class LessThanScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value < cmp_value; }
 
  protected:
+  /**
+   * Returns the value_id to a value which is the first value >= cmp_value
+   */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     return segment.lower_bound(cmp_value);
   }
@@ -260,6 +321,9 @@ class LessThanEqualsScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value <= cmp_value; }
 
  protected:
+  /**
+  * Returns the value_id to a value which is the first value > cmp_value
+  */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     return segment.upper_bound(cmp_value);
   }
@@ -275,6 +339,9 @@ class EqualsScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value == cmp_value; }
 
  protected:
+  /**
+  * Returns the value_id to cmp_value. Returns INVALID_VALUE_ID if cmp_value could not be found.
+  */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     auto value_id = segment.lower_bound(cmp_value);
     if (value_id != INVALID_VALUE_ID && cmp_value != segment.dictionary()->operator[](value_id)) {
@@ -294,6 +361,9 @@ class NotEqualsScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value != cmp_value; }
 
  protected:
+  /**
+  * Returns the value_id to cmp_value. Returns INVALID_VALUE_ID if cmp_value could not be found.
+  */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     auto value_id = segment.lower_bound(cmp_value);
     if (value_id != INVALID_VALUE_ID && cmp_value != segment.dictionary()->operator[](value_id)) {
@@ -314,6 +384,9 @@ class GreaterThanScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value > cmp_value; }
 
  protected:
+  /**
+  * Returns the value_id to a value which is the first value > cmp_value
+  */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     const auto value_id = segment.upper_bound(cmp_value);
     return value_id;
@@ -330,6 +403,9 @@ class GreaterThanEqualsScanner : public AbstractSegmentScanner<T> {
   bool compare(const T& value, const T& cmp_value) override { return value >= cmp_value; }
 
  protected:
+  /**
+  * Returns the value_id to a value which is the first value >= cmp_value
+  */
   ValueID get_value_id(const DictionarySegment<T>& segment, const T& cmp_value) override {
     const auto value_id = segment.lower_bound(cmp_value);
     return value_id;
--- src/lib/operators/table_scan.hpp
+++ src/lib/operators/table_scan.hpp
@@ -4,33 +4,15 @@
 
 #include "abstract_operator.hpp"
 #include "all_type_variant.hpp"
-#include "storage/table.hpp"
 #include "types.hpp"
-
-// TODO(anyone)
-/* - single chunk with referenceSegments that point to the found values
- * - do not produce empty chunks, unless the result is empty, in which case we produce a single empty chunk
- * - do not use operator[] on columns due to the virtual method calls involved and the use of AllTypeVariant
- * - therefore get the ValueSegments value vector or the DictionarySegments' att. vect. and the dict.
- *      when you want to scan the values.
- * - TableScan operator is not templated
- * - checking columns type: std::dynamic_pointer_cast<DictionarySegment>(b)
- *      if b can be casted to DictionarySegment, the pointer cast returns such a pointer,
- *      else it returns nullptr
- * - comparision: we will not deal with varying data types. BUT if the types do not match,
- *      the implementation should notice this and throw an exception.
- * - comparison is trivial if: T value_to_be_compared_to and the value vector const std::vector<T>& are available.
- * - ...
- */
 
 namespace opossum {
 
-class BaseTableScanImpl;
 class Table;
 
 class TableScan : public AbstractOperator {
  public:
-  TableScan(const std::shared_ptr<const AbstractOperator> in, ColumnID column_id, const ScanType scan_type,
+  explicit TableScan(const std::shared_ptr<const AbstractOperator> in, ColumnID column_id, const ScanType scan_type,
             const AllTypeVariant search_value);
 
   ColumnID column_id() const;
